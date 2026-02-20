47f2728aa674e0a5acc198592092572d52c0b768
Matty C. <matrimcn@gmail.com>
2026-02-20 16:25:29 -0500

    added euler-cromer update formulae to Ex. 1, but something isn't working.

--- Midterm Files/Midterm Ex 1_comp.py
+++ Midterm Files/Midterm Ex 1_comp.py
@@ -10,14 +10,18 @@ plt.style.use('seaborn-v0_8-colorblind')
 g = 9.81
 C_d = 0.25
 rho = 1.225
-A = 1.45E-3
-S = 0.2
+A = 6.4E-3
+S = 0.1
 m = 4.575E-2
 #set up constants
 
-vel_vec_init=[[77*cos(degrees(8.75)),0,77*sin(degrees(8.75))]]
-pos_vec_init=[[0,0,0]]
-omega_vec_init=[0,-330,0]
+vel_vec_aray = np.zeros((699,3))
+vel_vec_aray[0] = np.array([77*cos(radians(8.75)),0,77*sin(radians(8.75))])
+# use radians() function on degress.
+pos_vec_aray = np.zeros((699,3))
+pos_vec_aray[0]=np.array([0,0,0])
+omega_vec_aray = np.zeros((699,3))
+omega_vec_aray[0]=np.array([0,-130,0])
 #set up initial vectors
 
 delta_t = 1/100
@@ -25,10 +29,11 @@ t = np.arange(0,7,delta_t)
 #make 700 time points, from 1-7.
 
 def Magnitude(vec):
-    sq_vec=[]
+    sq_vec = np.zeros(len(vec))
     for i,j in enumerate(vec):
         sq_vec[i] = j**2
-    return np.sqrt(sum(sq_vec))
+        mag = np.sqrt(sum(sq_vec))
+    return mag
 #Define a simple magnitude function for any vector
 
 def Dot_v_x(vel_vec,omega_vec=[0,0,0],component=False):
@@ -60,11 +65,38 @@ def Dot_v_z(vel_vec,omega_vec=[0,0,0],component=False):
     magnus_a = -(S/m)*omega_vec[1]*vel_vec[0]
     #this one is negative
     if component==False:
-        return g_a+drag_a+magnus_a #dont forget gravity
+        return g_a+drag_a+magnus_a
     else:
         return (m*g_a,m*drag_a,m*magnus_a) #again, don't forget gravity
         #importantly, this one is a 3-tuple, instead of a 
         # 2-tuple, make sure future calls can handle that
 #Define our acceleration in z as a function of velocity
 
+acc_vec_aray = np.zeros((699,3))
+acc_vec_aray[0]=np.array([Dot_v_x(vel_vec_aray[0],omega_vec_aray[0]),
+               Dot_v_y(vel_vec_aray[0],omega_vec_aray[0]),
+               Dot_v_z(vel_vec_aray[0],omega_vec_aray[0])])
 
+for i in range(1,699):
+    vel_x = vel_vec_aray[i-1][0] + acc_vec_aray[i-1][0]*delta_t
+    vel_y = vel_vec_aray[i-1][1] + acc_vec_aray[i-1][1]*delta_t
+    vel_z = vel_vec_aray[i-1][2] + acc_vec_aray[i-1][2]*delta_t
+    vel_vec_aray[i] = np.array([vel_x,vel_y,vel_z])
+    #Euler velocity update
+    pos_x = pos_vec_aray[i-1][0] + vel_vec_aray[i][0]*delta_t
+    pos_y = pos_vec_aray[i-1][1] + vel_vec_aray[i][1]*delta_t
+    pos_z = pos_vec_aray[i-1][2] + vel_vec_aray[i][2]*delta_t
+    pos_vec_aray[i] = np.array([pos_x,pos_y,pos_z])
+    #Euler-Cromer position update
+    omega_y = omega_vec_aray[i-i][1]*0.964
+    omega_z = omega_vec_aray[i-1][2]*0.964
+    omega_vec_aray[i] = np.array([0,omega_y,omega_z])
+    #exponential decay of angular velocity
+    acc_x = Dot_v_x(vel_vec_aray[i],omega_vec_aray[i])
+    acc_y = Dot_v_y(vel_vec_aray[i],omega_vec_aray[i])
+    acc_z = Dot_v_z(vel_vec_aray[i],omega_vec_aray[i])
+    acc_vec_aray[i] = np.array([acc_x,acc_y,acc_z])
+    #Euler acceleration update
+
+print(vel_vec_aray[0:50])
+#This should print normal values and 100% SHOULD NOT diverge to NaN.
