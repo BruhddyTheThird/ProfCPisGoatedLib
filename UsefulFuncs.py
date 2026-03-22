1799e9af503854e3d4c857ee0bbd2b3135af34e8
Matty C. <matrimcn@gmail.com>
2026-03-22 15:07:35 -0400

    Small optimizations to functions

--- UsefulFuncs.py
+++ UsefulFuncs.py
@@ -1,5 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
+
 
 def SHO_acc(x,v):
     return -x
@@ -13,6 +12,7 @@ def RK2O_gen(x_0,v_0,t_0,tf,dt,acc_func=SHO_acc,fargs=tuple|None):
     an acceleration function (d^2x/dt^2) plus its additional arguments (fargs).
     Returns the time, position, and velocity arrays of the integrated equations of motion.
     """
+    import numpy as np
     #here, x,v are arbitrary parameters, we simply need a name for them.
     t = np.arange(t_0, tf, dt)
     x = np.zeros(t.shape)
@@ -37,7 +37,7 @@ def RK2O_gen(x_0,v_0,t_0,tf,dt,acc_func=SHO_acc,fargs=tuple|None):
         v[i] = v_prev + 0.5 * (k_1v+k_2v)
     return t,x,v
 
-def Generate_Phase_Space_gen(x_lim,v_lim,grid_size,acc_func=SHO_acc,fargs=tuple|None):
+def Gen_Phase_Space(x_lim,v_lim,grid_size,acc_func=SHO_acc,fargs=tuple|None):
     """
     Generates a phase space mesh and vectors for a given acceleration function.
     Takes the limits of the limits of the variables (x_lim, v_lim),
@@ -45,14 +45,15 @@ def Generate_Phase_Space_gen(x_lim,v_lim,grid_size,acc_func=SHO_acc,fargs=tuple|
     and an acceleration function plus its arguments.
     Returns the grid 'X,V' and the changes in variable values at each point, 'dX, dV'.
     """
+    import numpy as np
     X_space = np.linspace(x_lim[0],x_lim[1], grid_size)
-    V_space = np.linspace(v_lim[0], v_lim[1], grid_size)
-# intitalize our axes
+    V_space = np.linspace(v_lim[0],v_lim[1], grid_size)
+    # intitalize our axes
     X,V = np.meshgrid(X_space,V_space)
-# combine axes
+    # combine axes
     try:
         dX,dV = V, acc_func(X,V,*fargs)
     except:
         dX,dV = V, acc_func(X,V)
-# calculate change in axes values
+    # calculate change in axes values
     return X,V,dX,dV
