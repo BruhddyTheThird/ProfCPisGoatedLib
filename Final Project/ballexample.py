aac6f3da68ac1c4feff0663728405da06c780d51
Matty C. <matrimcn@gmail.com>
2026-04-20 14:56:36 -0400

    Example almost done, updating forces.

--- Final Project/ballexample.py
+++ Final Project/ballexample.py
@@ -1,13 +1,11 @@
-#THIS IS COPIED FROM https://github.com/viblo/pymunk/blob/master/pymunk/examples/bouncing_balls.py !!!
-
-"""This example spawns (bouncing) balls randomly on a L-shape constructed of
-two segment shapes. Not interactive.
-"""
+#SOME OF THIS IS COPIED FROM https://github.com/viblo/pymunk/blob/master/pymunk/examples/bouncing_balls.py !!!
 
 __docformat__ = "reStructuredText"
 
 # Python imports
 import random
+import time
+import sys
 
 # Library imports
 import pygame
@@ -17,23 +15,28 @@ import numpy as np
 import pymunk
 import pymunk.pygame_util
 
-
-class BouncyBalls(object):
+done_saving = pygame.USEREVENT + 1 # Make a new user event
+saving = False #initialize global saving for ball_math
+total_impulse = [0,0]
+class RigidAirSimulation(object):
     """
-    This class implements a simple scene in which there is a static platform (made up of a couple of lines)
-    that don't move. Balls appear occasionally and drop onto the platform. They bounce around.
+    This class implements a simulation of air on a rotating or static ball.
+    Air starts on one side, and travels to the other with an initial velocity determined by its random position.
+    Outputs to console (for now) when air interacts with the large central ball.
     """
 
     def __init__(self) -> None:
         # Space
         self._space = pymunk.Space()
-        self._space.gravity = (0, 0)
 
         # Physics
         # Time step
         self._dt = (0.5) / 60.0
         # Number of physics steps per screen frame
-        self._physics_steps_per_frame = 10
+        self._physics_steps_per_frame = 4
+
+        #Important collision handling
+        self._space.on_collision(1,2,begin=self._ball_show,post_solve=self._ball_math)
 
         # pygame
         pygame.init()
@@ -41,14 +44,14 @@ class BouncyBalls(object):
         self._clock = pygame.time.Clock()
 
         self._draw_options = pymunk.pygame_util.DrawOptions(self._screen)
+        self._draw_options.flags = pymunk.pygame_util.DrawOptions.DRAW_SHAPES
 
         # Static barrier walls (lines) that the balls bounce off of
         self._add_static_scenery()
+        # Static/rotating golf ball that the balls hit.
         self._add_golf_ball()
-
         # Balls that exist in the world
         self._balls: list[pymunk.Circle] = []
-
         # Execution control and time until the next ball spawns
         self._running = True
         self._ticks_to_next_ball = 1
@@ -73,7 +76,6 @@ class BouncyBalls(object):
             self._clock.tick(60)
             fps = self._clock.get_fps()
             pygame.display.set_caption("fps: " + f"{fps:.2f}")
-
     def _add_static_scenery(self) -> None:
         """
         Create the static bodies.
@@ -90,20 +92,21 @@ class BouncyBalls(object):
         self._space.add(*static_lines)
     
     def _add_golf_ball(self) -> None:
-        son = "SON D:"
+        """SON D:"""
         body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
         body.position = 150,210
         body.angular_velocity = 2*np.pi
         shape = pymunk.Circle(body,50)
         shape.elasticity = 0.95
         shape.friction = 0.25
+        shape.collision_type = 2 #golf ball...
         self._space.add(body,shape)
-
     def _process_events(self) -> None:
         """
         Handle game and events like keyboard input. Call once per frame only.
         :return: None
         """
+        global saving
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self._running = False
@@ -111,7 +114,16 @@ class BouncyBalls(object):
                 self._running = False
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
                 pygame.image.save(self._screen, "bouncing_balls.png")
-
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+                saving = True #flag for saving frames 
+                pygame.time.set_timer(done_saving,10000) # set timer for event to trigger in 10000 ms
+                print("Saving next 10000ms") 
+            elif event.type == done_saving:
+                saving = False #update flag
+                force = np.array(total_impulse) / 10000
+                print("Save complete")
+                print(f"Force is {force[0]:.2e} m/s? in x-direction.\nForce is {force[1]:.2e} m/s? in y-direction.")
+                pygame.time.set_timer(done_saving, 0) # is this necessary? ideally the timer should stop automatically, since set_timer was called with loops=0.
     def _update_balls(self) -> None:
         """
         Create/remove balls as necessary. Call once per frame only.
@@ -120,11 +132,11 @@ class BouncyBalls(object):
         self._ticks_to_next_ball -= 1
         if self._ticks_to_next_ball <= 0:
             i = 1
-            while i < 40:
+            while i < 40: #create 40 air "molecules" per tick
                 self._create_ball()
                 i += 1
             self._ticks_to_next_ball = 1
-        # Remove balls that move farther than 1000 horizontally
+        # Remove balls that move farther than bounding box horizontally
         balls_to_remove = [ball for ball in self._balls if (ball.body.position.x > 300 or ball.body.position.x < 10)]
         for ball in balls_to_remove:
             self._space.remove(ball, ball.body)
@@ -136,9 +148,9 @@ class BouncyBalls(object):
         :return:
         """
         mass = 5.3E-9  # in kg
-        radius = 1 #.42E-4  in meters
+        radius = 1 # in mm
         inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
-        body = pymunk.Body(mass, float(inertia)) #testing 0 moment
+        body = pymunk.Body(mass, inertia) #testing 0 moment
         x = 10
         y = random.randint(101,600-281)
         body.position = x, y
@@ -146,6 +158,7 @@ class BouncyBalls(object):
         shape = pymunk.Circle(body, radius, (0, 0))
         shape.elasticity = 1
         shape.friction = 0
+        shape.collision_type = 1
         self._space.add(body, shape)
         self._balls.append(shape)
 
@@ -162,12 +175,33 @@ class BouncyBalls(object):
         :return: None
         """
         self._space.debug_draw(self._draw_options)
+    def _ball_show(self,arbiter,space,data):
+        """
+        Say beginning of collision. Test. \n
+        Returns True
+        """
+        hit_end_time = time.perf_counter()
+        hit_time = hit_end_time - start_sim_time
+        #print(f"Collision occured with golf ball at {hit_time:.2f} seconds!")
+        return True
+    def _ball_math(self,arbiter,space,data) -> None:
+        """
+        Access impulse on ball due to collision (post_solve).
+        Add impulse (equals $\Delta p$) to total momentum change, 
+        """
+        global total_impulse
+        if saving == True:
+            old_total_impulse = pymunk.vec2d.Vec2d(*total_impulse)
+            total_impulse = list(arbiter.total_impulse + old_total_impulse)
+            print("It matters! It does matter!!")
+
 
 
 def main():
-    game = BouncyBalls()
+    game = RigidAirSimulation()
     game.run()
 
+start_sim_time = time.perf_counter()
 
 if __name__ == "__main__":
     main()
